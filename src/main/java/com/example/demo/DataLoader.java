6e4a0816a745e89d05a33aa7964d379286ff0cd4
pierz barry <pmbarry@asu.edu>
2019-11-21 16:55:33 -0500

    fixed a lot

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -14,7 +14,17 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         Flight flight;
-        flight = new Flight("8:00AM BWI", "4:00PM LAX", "oneway", "$450", "June 24th", "8036", "Delta", null);
+
+        flight = new Flight("8:00AM BWI", "4:00PM LAX", "oneway", "450", "June 24th", "8036", "Delta", null);
+        repository.save(flight);
+
+        flight = new Flight("6:00AM BWI", "1:00PM LAX", "oneway", "390", "June 26th", "8346", "United", null);
+        repository.save(flight);
+
+        flight = new Flight("10:00AM BWI", "8:00PM LAX", "oneway", "320", "June 25th", "3236", "Spirit", null);
+        repository.save(flight);
+
+        flight = new Flight("1:00AM BWI", "1:00PM LAX", "oneway", "420", "August 5th", "0036", "Delta", null);
         repository.save(flight);
     }
 }
